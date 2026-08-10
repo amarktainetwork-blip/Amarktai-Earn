72afb57c1cd63b59e459aec739a56d858f5630ba
amarktainetwork-blip <amarktainetwork@gmail.com>
2026-08-10 10:58:37 +0200

    fix: bootstrap revenue catalog on web startup

--- scripts/start-web.sh
+++ scripts/start-web.sh
@@ -2,5 +2,6 @@
 set -eu
 python manage.py production_check
 python manage.py migrate --noinput
+python manage.py bootstrap_revenue_catalog
 python manage.py collectstatic --noinput
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers "${GUNICORN_WORKERS:-3}" --timeout "${GUNICORN_TIMEOUT:-120}"
